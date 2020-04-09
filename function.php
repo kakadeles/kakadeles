0fd66dd95dba2173447a32574c74d7c9f822013d
kakadeles <61360461+kakadeles@users.noreply.github.com>
2020-04-09 10:53:59 +0700

    function.php

--- function.php
+++ function.php
@@ -1,100 +1,83 @@
-<?php
-
-function request($url, $token = null, $data = null, $pin = null, $otpsetpin = null, $uuid = null){
-
-$header[] = "Host: api.gojekapi.com";
-$header[] = "User-Agent: okhttp/3.10.0";
-$header[] = "Accept: application/json";
-$header[] = "Accept-Language: id-ID";
-$header[] = "Content-Type: application/json; charset=UTF-8";
-$header[] = "X-AppVersion: 3.30.2";
-$header[] = "X-UniqueId: ".time()."57".mt_rand(1000,9999);
-$header[] = "Connection: keep-alive";
-$header[] = "X-User-Locale: id_ID";
-$header[] = "X-Location: -6.917464,107.619122";
-$header[] = "X-Location-Accuracy: 3.0";
-if ($pin):
-$header[] = "pin: $pin";
-    endif;
-if ($token):
-$header[] = "Authorization: Bearer $token";
-endif;
-if ($otpsetpin):
-$header[] = "otp: $otpsetpin";
-endif;
-if ($uuid):
-$header[] = "User-uuid: $uuid";
-endif;
-$c = curl_init("https://api.gojekapi.com".$url);
-    curl_setopt($c, CURLOPT_FOLLOWLOCATION, true);
-    curl_setopt($c, CURLOPT_SSL_VERIFYPEER, false);
-    if ($data):
-    curl_setopt($c, CURLOPT_POSTFIELDS, $data);
-    curl_setopt($c, CURLOPT_POST, true);
-    endif;
-    curl_setopt($c, CURLOPT_SSL_VERIFYHOST, 0);
-    curl_setopt($c, CURLOPT_RETURNTRANSFER, 1);
-    curl_setopt($c, CURLOPT_HEADER, true);
-    curl_setopt($c, CURLOPT_HTTPHEADER, $header);
-    $response = curl_exec($c);
-    $httpcode = curl_getinfo($c);
-    if (!$httpcode)
-        return false;
-    else {
-        $header = substr($response, 0, curl_getinfo($c, CURLINFO_HEADER_SIZE));
-        $body   = substr($response, curl_getinfo($c, CURLINFO_HEADER_SIZE));
-    }
-    $json = json_decode($body, true);
-    return $body;
-}
-function save($filename, $content)
-{
-    $save = fopen($filename, "a");
-    fputs($save, "$content\r\n");
-    fclose($save);
-}
-function nama()
-    {
-    $ch = curl_init();
-    curl_setopt($ch, CURLOPT_URL, "http://ninjaname.horseridersupply.com/indonesian_name.php");
-    curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, 0);
-    curl_setopt($ch, CURLOPT_SSL_VERIFYHOST, 0);
-    curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
-    curl_setopt($ch, CURLOPT_FOLLOWLOCATION, 1);
-    $ex = curl_exec($ch);
-    // $rand = json_decode($rnd_get, true);
-    preg_match_all('~(&bull; (.*?)<br/>&bull; )~', $ex, $name);
-    return $name[2][mt_rand(0, 14) ];
-    }
-function getStr($a,$b,$c){
-	$a = @explode($a,$c)[1];
-	return @explode($b,$a)[0];
-}
-function getStr1($a,$b,$c,$d){
-        $a = @explode($a,$c)[$d];
-        return @explode($b,$a)[0];
-}
-function color($color = "default" , $text)
-    {
-        $arrayColor = array(
-            'grey'      => '1;30',
-            'red'       => '1;31',
-            'green'     => '1;32',
-            'yellow'    => '1;33',
-            'blue'      => '1;34',
-            'purple'    => '1;35',
-            'nevy'      => '1;36',
-            'white'     => '1;0',
-        );  
-        return "\033[".$arrayColor[$color]."m".$text."\033[0m";
-    }
-function fetch_value($str,$find_start,$find_end) {
-	$start = @strpos($str,$find_start);
-	if ($start === false) {
-		return "";
-	}
-	$length = strlen($find_start);
-	$end    = strpos(substr($str,$start +$length),$find_end);
-	return trim(substr($str,$start +$length,$end));
-}
-?>
+date_default_timezone_set('Asia/Jakarta');
+include "function.php";
+os.system('clear');
+sleep (2);
+echo color("blue"," Silahkan Tunggu Sebentar \n");
+sleep (2);
+echo color("red"," Subscribe Arkan Channel Dulu Jangan Lupa ! \n");
+os.system('xdg-open https://m.youtube.com/channel/UCr8L7Fim8vvG71kbvqytFZQ');
+sleep (5);
+os.system('clear');
+sleep (2);
+echo color("blue"," █████████████████████████████████████████████ \n");
+echo color("yellow","[•] Time   : ".date('[d-m-Y] [H:i:s]')."     [•] \n");
+echo color("red","●▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬๑๑▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬● \n");
+echo color("green","[√] Version 1.3                     [√] \n");
+echo color("red","●▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬๑๑▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬● \n");
+echo color("green","[•] Nama Tools : Tembak Voucher Go Food  [•] \n");
+echo color("red","[•]      SUBSCRIBE ARKAN CHANNEL         [•]  \n");
+echo color("blue","[•] Gunakan Tools Ini Dengan Bijak Oke ! [•] \n");
+echo color("yellow","[•] Penulisan Nomor Yang Benar 62xxxxxxxx[•] \n");
+echo color("blue"," █████████████████████████████████████████████\n");
+function change(){
+        $nama = nama();
+        $email = str_replace(" ", "", $nama) . mt_rand(100, 999);
+        ulang:
+        echo color("yellow","(√) Nomor : ");
+        $no = trim(fgets(STDIN));
+        $data = '{"email":"'.$email.'@gmail.com","name":"'.$nama.'","phone":"+'.$no.'","signed_up_country":"ID"}';
+        $register = request("/v5/customers", null, $data);
+        if(strpos($register, '"otp_token"')){
+        $otptoken = getStr('"otp_token":"','"',$register);
+        echo color("green","+] Kode verifikasi sudah di kirim")."\n";
+        otp:
+        echo color("red","?] Otp: ");
+        $otp = trim(fgets(STDIN));
+        $data1 = '{"client_name":"gojek:cons:android","data":{"otp":"' . $otp . '","otp_token":"' . $otptoken . '"},"client_secret":"83415d06-ec4e-11e6-a41b-6c40088ab51e"}';
+        $verif = request("/v5/customers/phone/verify", null, $data1);
+        if(strpos($verif, '"access_token"')){
+        echo color("yellow","+] Berhasil mendaftar");
+        $token = getStr('"access_token":"','"',$verif);
+        $uuid = getStr('"resource_owner_id":',',',$verif);
+        echo "\n".color("yellow","+] Your access token : ".$token."\n\n");
+        save("token.txt",$token);
+        echo "\n".color("nevy","?] Mau Redeem Voucher?: y/n ");
+        $pilihan = trim(fgets(STDIN));
+        if($pilihan == "y" || $pilihan == "Y"){
+        echo color("red","===========(REDEEM VOUCHER)===========");
+        echo "\n".color("blue","!] Claim voc GO FOOD 20K");
+        echo "\n".color("yellow","!] Please wait");
+        for($a=1;$a<=3;$a++){
+        echo color("yellow",".");
+        sleep(3);
+        }
+        $code1 = request('/go-promotions/v1/promotions/enrollments', $token, '{"promo_code":"COBAGOFOOD010420B"}');
+        $message = fetch_value($code1,'"message":"','"');
+        if(strpos($code1, 'Promo kamu sudah bisa dipakai')){
+        echo "\n".color("green","+] Message: ".$message);
+        goto gocar;
+        }else{
+        echo "\n".color("red","-] Message: ".$message);
+        gocar:
+        echo "\n".color("blue","!] Claim voc GOFOOD 15+10+5");
+        echo "\n".color("yellow","!] Please wait");
+        for($a=1;$a<=3;$a++){
+        echo color("yellow",".");
+        sleep(20);
+        }
+        $code1 = request('/go-promotions/v1/promotions/enrollments', $token, '{"promo_code":"COBAGOFOOD010420A"}');
+        $message = fetch_value($code1,'"message":"','"');
+        if(strpos($code1, 'Promo kamu sudah bisa dipakai')){
+        echo "\n".color("green","+] Message: ".$message);
+        goto gofood;
+        }else{
+        echo "\n".color("red","-] Message: ".$message);
+        reff:
+        $data = '{"referral_code":"G-75SR565"}';    
+        $claim = request("/customer_referrals/v1/campaign/enrolment", $token, $data);
+        $message = fetch_value($claim,'"message":"','"');
+        if(strpos($claim, 'Promo kamu sudah bisa dipakai')){
+        echo "\n".color("green","+] Message: ".$message);
+        goto gofood;
+        }else{
+        echo "\n".color("red","-] Message: ".$message
